1e7f16c81ec7252c5606e92e4499410872717763
Dimitar Roussev <dimitarrussev@gmail.com>
2020-09-18 21:25:01 +0300

    Added a runner

--- src/main/java/com/lecture13/homework/Main.java
+++ src/main/java/com/lecture13/homework/Main.java
@@ -1,2 +1,18 @@
-package com.lecture13.homework;public class Main {
+package com.lecture13.homework;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        SoleProprietor sp = new SoleProprietor(
+                "ЕТ Чугун99",
+                "19.9.1999",
+                "1028528710",
+                "Желязко Каменов",
+                10000,
+                99999);
+        log.info("Company profit is {}", sp.calculateProfit());
+    }
+
 }
